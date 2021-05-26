30ab1812f9d329d7874015154746671310e182b0
kylianjamie <kjnijnuis@gmail.com>
2021-05-26 15:24:36 +0200

    test loading manager

--- app.js
+++ app.js
@@ -198,8 +198,36 @@ class App{
             this.initAR();
         }
         
-		const loader = new GLTFLoader( ).setPath(this.assetsPath);
+		const loader = new GLTFLoader(manager).setPath(this.assetsPath);
         const self = this;
+
+        // loader
+        const manager = new THREE.LoadingManager();
+        manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+
+            console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+
+        };
+
+        manager.onLoad = function ( ) {
+
+            console.log( 'Loading complete!');
+
+        };
+
+
+        manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+
+            console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+
+        };
+
+        manager.onError = function ( url ) {
+
+            console.log( 'There was an error loading ' + url );
+
+        };
+
         
         this.loadingBar.visible = true;
 		
